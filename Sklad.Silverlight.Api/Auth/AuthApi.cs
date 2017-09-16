2abdd53d411a6ce7c0b079d49f1b3e584c00fe87
Yauheni Khvaliuk <hvaluk@gmail.com>
2017-09-16 21:42:29 +0200

    add api project

---
+++ Sklad.Silverlight.Api/Auth/AuthApi.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using RestSharp;
+
+namespace Sklad.Silverlight.Api.Auth
+{
+    public class AuthApi
+    {
+        private readonly string baseUrl;
+
+        public AuthApi(string baseUrl)
+        {
+            this.baseUrl = baseUrl;
+        }
+
+        public Task<bool> Login(string login, string password)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest("/Token");
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            client.ExecuteAsync(request, response =>
+            {
+                tcs.SetResult(false);
+            });
+
+            return tcs.Task;
+        }
+    }
+}
---
+++ Sklad.Silverlight.Api/Auth/AuthToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sklad.Silverlight.Api.Auth
+{
+    public class AuthToken
+    {
+        public string AccessToken { get; set; }
+
+        public DateTime Expires { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Role { get; set; }
+    }
+}
